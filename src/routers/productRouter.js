bc9b95befa6268103425b5445795d0a268ecc121
joabe <joabe9999pereira@gmail.com>
2023-02-27 20:24:18 -0300

    requisito 4

--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { productController } = require('../controllers');
 
+const { nameValidation } = require('../middlewares/nameValidation');
+
 const router = express.Router();
 
 router.get('/', productController.getAllProducts);
 
 router.get('/:id', productController.getProductById);
 
-router.post('/', productController.newProduct);
+router.post('/', nameValidation, productController.newProduct);
 module.exports = router;
