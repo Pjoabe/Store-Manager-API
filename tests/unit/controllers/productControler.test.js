d8b81634ec53c31249a26ebed7a165da4c00f5ad
joabe <joabe9999pereira@gmail.com>
2023-03-02 20:28:40 -0300

    testes e mais testes

--- tests/unit/controllers/productControler.test.js
+++ tests/unit/controllers/productControler.test.js
@@ -10,6 +10,7 @@ const { productController } = require('../../../src/controllers');
 const {
   allProductsResponse,
   productSearchNameResponse,
+  error404,
 } = require('../../../__tests__/_dataMock');
 
 describe("test the controller layer", function () {
@@ -28,9 +29,32 @@ describe("test the controller layer", function () {
     expect(res.json).to.have.been.calledWith(productSearchNameResponse[0]);
   });
 
-  it('testando, apenas', async function () {
+it("should throw 404 error", async function () {
+  const res = {};
+  const req = { params: { id: 24 } };
+  res.status = sinon.stub().returns(res);
+  res.json = sinon.stub().returns();
+  sinon.stub(productService, "productById").resolves(error404);
+  await productController.getProductById(req, res);
 
-  });
+  expect(res.status).to.have.been.calledWith(404);
+
+  expect(res.json).to.have.been.calledWith(error404);
+});
+
+ it("should search for all products", async function () {
+   const res = {};
+   const req = {};
+   res.status = sinon.stub().returns(res);
+   res.json = sinon.stub().returns();
+   sinon.stub(productService, "allProducts").resolves(allProductsResponse);
+   await productController.getAllProducts(req, res);
+
+   expect(res.status).to.have.been.calledWith(200);
+
+   expect(res.json).to.have.been.calledWith(allProductsResponse);
+ });
+
   afterEach(function () {
     sinon.restore();
   });
