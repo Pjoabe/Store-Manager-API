e7dd7a61816df0a98fe356d717e4e38e9cf32fbc
joabe <joabe9999pereira@gmail.com>
2023-03-03 11:40:12 -0300

    seguindo a dica do naza

--- tests/unit/services/productService.test.js
+++ tests/unit/services/productService.test.js
@@ -3,7 +3,7 @@ const sinon = require('sinon');
 const { productModel } = require('../../../src/models');
 const { productService } = require('../../../src/services');
 const connection = require('../../../src/models/conection');
-const { allProductsResponse,error404, products } = require('../../../__tests__/_dataMock');
+const { allProductsResponse,error404 } = require('../../../__tests__/_dataMock');
 
 describe("test the service layer", function () {
   it("Should return an error when searching for a product by its id", async function () {
