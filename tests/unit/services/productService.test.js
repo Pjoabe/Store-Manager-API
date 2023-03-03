4d090249232c0821d5626b782ec4ff11e7a8604f
joabe <joabe9999pereira@gmail.com>
2023-03-03 02:57:08 -0300

    mais testes, no local passa...

--- tests/unit/services/productService.test.js
+++ tests/unit/services/productService.test.js
@@ -3,7 +3,7 @@ const sinon = require('sinon');
 const { productModel } = require('../../../src/models');
 const { productService } = require('../../../src/services');
 const connection = require('../../../src/models/conection');
-const { allProductsResponse,error404 } = require('../../../__tests__/_dataMock');
+const { allProductsResponse,error404, products } = require('../../../__tests__/_dataMock');
 
 describe("test the service layer", function () {
   it("Should return an error when searching for a product by its id", async function () {
@@ -27,7 +27,21 @@ describe("test the service layer", function () {
     expect(result).to.be.deep.equal(allProductsResponse);
   });
 
+  it("should register a new product", async function () {
+    sinon.stub(productModel, "insertSQL").resolves(5);
+    sinon.stub(productModel, "getById").resolves([allProductsResponse[2]]);
+
+    const result = await productService.newInsertion("ProdutoX");
+
+    expect(result).to.be.deep.equal([allProductsResponse[2]]);
+  });
+
+ it("should throw 404 error about wrong product insertion", async function () {
+
+ });
+
   afterEach(function () {
     sinon.restore();
   });
+
 });
